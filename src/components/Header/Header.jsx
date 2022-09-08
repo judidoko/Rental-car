be4eba1c27b3f3f746d6eae14b0801fac4fcb27a
judidoko <judidoko@gmail.com>
2022-09-08 15:06:08 +0100

    edited header rental

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link, NavLink } from "react-router-dom";
 import "../../styles/Header.css";
